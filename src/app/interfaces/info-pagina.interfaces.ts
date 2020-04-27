3115222d31b467fd4f45d01a906acf3023e246da
Noelia <noeliam@bravent.net>
2020-04-27 17:40:28 +0200

    imagenes e iconos

---
+++ src/app/interfaces/info-pagina.interfaces.ts
@@ -0,0 +1,12 @@
+
+export interface InfoPagina {
+ titulo?: string;
+ autora?: string;
+  email?: string;
+  nombre_corto?: string;
+  linkendin?: string;
+  facebook?: string;
+  twitter?: string;
+  github?: string;
+  equipo?: any[];
+}
